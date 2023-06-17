fccd85078273ebef998d53447742fcfec4546c7e
Mahmoud Elshall <54082956+mahmoudelshall@users.noreply.github.com>
2023-06-17 15:02:35 +0200

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import fetch from "node-fetch";
 
 
-let categorizeProducts = (data)=>{
+let categorizeProducts = (data,rate)=>{
     let allCategories  = data.map(item => {
         return {
           "id" : item.category.id,
@@ -17,6 +17,7 @@ let categorizeProducts = (data)=>{
           buckets.push({"category" :item,"products":[]})
        })
        data.forEach(item=>{
+        item.price = item.price * rate;
           for (const key in buckets) {
               if(item.category.id == buckets[key].category.id ){
                   buckets[key].products.push(item)   
@@ -28,9 +29,13 @@ let categorizeProducts = (data)=>{
          return buckets;
 } 
 
-
+// get products
 const response = await fetch('https://api.escuelajs.co/api/v1/products');
 const data = await  response.json();
-let buckets = categorizeProducts(data);
+// get  currancy rate of EGY
+const rate = await fetch("https://api.exchangerate.host/latest?base=USD").then((res) => res.json()).then((data) => data.rates['EGP']);
+
+
+let buckets = categorizeProducts(data,rate);
 
 console.log(buckets);
